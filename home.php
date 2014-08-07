8132d0fc78c9abfa25d1ccd774d163eab0484b69
Tommy Marshall <tommy.marshall@viget.com>
2014-08-07 15:24:32 -0400

    Initial commit.

---
+++ home.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>BrkPnt.com | Showing Your Breakpoint</title>
+	<link href="//maxcdn.bootstrapcdn.com/bootswatch/3.2.0/superhero/bootstrap.min.css" rel="stylesheet">
+	<link href="styles.css" rel="stylesheet">
+</head>
+<body>
+
+	<div class="container">
+		<div class="header">
+			<h1>
+				BrkPnt<span>.com</span>
+				<em>Showing Your Breakpoint</em>
+			</h1>
+		</div>
+
+		<div class="jumbotron">
+			<p class="lead">
+				Whether you're working on buildout or doing QA, it's tough to know the breakpoint you're viewing a page at. Add this customizable snippet to your page and stop wondering.
+			</p>
+			<code>
+				&lt;link href="//brkpnt.com/mobile:0,tablet:501,desktop:1024" rel="stylesheet"&gt;
+			</code>
+		</div>
+		<h3>How does it work?</h3>
+		<hr>
+		<p>The above link tag styles the <em>:after</em> pseudo-element of the body with breakpoints you define in the URL. It then changes the <em>content</em> of that pseudo-element with the name of the specified breakpoint. Done.</p>
+		<p style="text-align: center">
+			<a class="btn btn-success" role="button" href="http://viget.com/extend/check-your-breakpoint-using-this-simple-css-snippet" target="_blank">Read more on how it works</a>
+		</p>
+		<h3>Other examples</h3>
+		<hr>
+		<p>Specify min and max widths using a dash.</p>
+		<code>
+			&lt;link href="//brkpnt.com/mobile:0-500,tablet:501-800" rel="stylesheet"&gt;
+		</code>
+		<p>Specify heights by adding <strong>h</strong>, widths are the default.</p>
+		<code>
+			&lt;link href="//brkpnt.com/mobile:0h,tablet:501h,cinema:980h" rel="stylesheet"&gt;
+		</code>
+		<p>Change the color theme.</p>
+		<code>
+			<span>// Blue theme</span><br>
+			&lt;link href="//brkpnt.com/mobile:0,notebook:800|blue" rel="stylesheet"&gt;
+			<br><br><span>// Red theme</span><br>
+			&lt;link href="//brkpnt.com/mobile:0,notebook:800|red" rel="stylesheet"&gt;
+		</code>
+		<h3>Credits</h3>
+		<hr>
+		<p>Created by <a href="http://twitter.com/tommyjmarshall">Tommy Marshall</a> as a <a href="http://pointlesscorp.com">Pointless Stunt</a> on behalf of <a href="http://viget.com/" target="_blank">Viget</a>.</p>
+
+		<div class="footer">
+			BrkPnt.com &copy; 2014
+		</div>
+
+	</div>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,88 @@
+<?php
+
+// Get Route
+$route = substr($_SERVER['REQUEST_URI'], 1);
+
+if (strpos($route,':') !== false) {
+	// Assign Breakpoint
+	$theme = explode('|', $route);
+	$breakpoints = explode(',', $theme[0]);
+
+	// Assign Breakpoint values
+	foreach ($breakpoints as $data) {
+		$breakpoint_values = explode(':', $data);
+		$breakpoint_name = $breakpoint_values[0];
+		$breakpoint_data = explode('-', $breakpoint_values[1]);
+
+		// Assign Min and Max breakpoint
+		$min = $breakpoint_data[0];
+		$max = $breakpoint_data[1];
+
+		// height or width?
+		if (strtolower(substr($min, -1)) == "h") {
+			$first_type = 'height';
+			$min = substr($min, 0, -1);
+		} else {
+			$first_type = 'width';
+		}
+
+		$max_string = "";
+		if ( $max ) {
+			$second_type = strtolower(substr($max, -1)) == "h" ? 'height' : 'width';
+			$max = substr($max, 0, -1);
+			$max_string =  " and (max-{$second_type}: {$max}px)";
+		}
+
+		$output .= "\n@media (min-{$first_type}: {$min}px){$max_string}{\n";
+		$output .= "	body:after{\n";
+		$output .= "		content: \"$breakpoint_name\";\n";
+		$output .= "	}\n";
+		$output .= "}\n";
+	}
+
+	// Return CSS
+	$themes = array(
+		'default' => '
+	background: #a12347;
+	color: #fff;
+	font-family: "Helvetica Nueu", Helvetica, Arial;
+	font-size: 22px;
+	font-weight: bold;
+	text-transform: uppercase;
+	padding: 5px 10px;
+	border-bottom-left-radius: 3px;
+		',
+		'blue' => '
+	background: #2347a1;
+	color: #fff;
+	font-family: "Helvetica Nueu", Helvetica, Arial;
+	font-size: 22px;
+	font-weight: bold;
+	text-transform: uppercase;
+	padding: 5px 10px;
+	border-bottom-left-radius: 3px;
+		'
+	);
+
+	if ($theme[1] && key_exists($theme[1], $themes)) {
+		$theme = $themes[$theme[1]];
+	} else {
+		$theme = $themes['default'];
+	}
+
+	header("Content-Type: text/css", true);
+	echo "
+body:after {
+	box-sizing: border-box;
+	position: fixed;
+	right: 0;
+	text-align: center;
+	top: 0;
+	z-index: 99;
+	$theme
+}
+$output
+";
+} else {
+	include 'home.php';
+}
