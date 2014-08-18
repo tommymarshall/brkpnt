0ed5ea8ee3229ba83a398640b93d32bf34f1b1bd
Tommy Marshall <tommy.marshall@viget.com>
2014-08-18 16:47:47 -0400

    Adds GA tracking.

--- public/home.php
+++ public/home.php
@@ -7,12 +7,21 @@
 	<link href="/styles.css" rel="stylesheet">
 
 	<!--
-		HAI! FYI, the stylesheet below is and example
-		of how BrkPnt works. Such easy.
+		HAI! FYI, the stylesheet below is an
+		example of how BrkPnt works. Such easy.
 	-->
 	<link href="//brkpnt.com/mobile:0,tablet:768,desktop:1200" rel="stylesheet">
 </head>
 <body>
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-53965300-1', 'auto');
+		ga('send', 'pageview');
+	</script>
 	<div class="check-it-out">
 		<p>
 			<span>Resize window</span>
