f499d808f30ae419444afbc15c1bb1ee48320280
Tommy Marshall <tommy.marshall@viget.com>
2014-09-23 11:29:50 -0400

    Convert to spaces.

--- public/index.php
+++ public/index.php
@@ -2,164 +2,161 @@
 
 class MediaQueryBuilder {
 
-	protected $_request;
+    protected $_request;
 
-	protected $_parameters;
+    protected $_parameters;
 
-	protected $_breakpoints = [];
+    protected $_breakpoints = [];
 
-	// Weird indentation to account for output
-	protected $_themes = [
-		'default' => '
-	/* Red theme */
-	background: #a12347;
-	color: #fff;
-	font-family: "Helvetica Nueu", Helvetica, Arial;
-	font-size: 22px;
-	font-weight: bold;
-	text-transform: uppercase;
-	padding: 5px 10px;
-	border-bottom-left-radius: 3px;
-		',
-		'blue' => '
-	/* Blue theme */
-	background: #2347a1;
-	color: #fff;
-	font-family: "Helvetica Nueu", Helvetica, Arial;
-	font-size: 22px;
-	font-weight: bold;
-	text-transform: uppercase;
-	padding: 5px 10px;
-	border-bottom-left-radius: 3px;
-		'
-	];
+    // Weird indentation to account for output
+    protected $_themes = [
+        'default' => '
+    /* Red theme */
+    background: #a12347;
+    color: #fff;
+    font-family: "Helvetica Nueu", Helvetica, Arial;
+    font-size: 22px;
+    font-weight: bold;
+    text-transform: uppercase;
+    padding: 5px 10px;
+    border-bottom-left-radius: 3px;
+        ',
+        'blue' => '
+    /* Blue theme */
+    background: #2347a1;
+    color: #fff;
+    font-family: "Helvetica Nueu", Helvetica, Arial;
+    font-size: 22px;
+    font-weight: bold;
+    text-transform: uppercase;
+    padding: 5px 10px;
+    border-bottom-left-radius: 3px;
+        '
+    ];
 
-	public function __construct($request)
-	{
+    public function __construct($request)
+    {
+        $this->_request    = $this->cleanRequest($request);
+        $this->_parameters = $this->getBreakpointParameters($request);
+        $this->_themeName  = $this->getThemeName($request);
+        $this->build();
+    }
 
-		$this->_request    = $this->cleanRequest($request);
-		$this->_parameters = $this->getBreakpointParameters($request);
-		$this->_themeName  = $this->getThemeName($request);
-		$this->build();
-	}
+    public function cleanRequest($request)
+    {
+        return trim(substr($request, strrpos($request, '/')));
+    }
 
-	public function cleanRequest($request)
-	{
-		return trim(substr($request, strrpos($request, '/')));
-	}
+    public function build()
+    {
+        // Assign Breakpoint values
+        foreach ($this->_parameters as $data) {
+            $output = "@media ";
 
-	public function build()
-	{
-		// Assign Breakpoint values
+            $breakpoint_definition = explode(':', $data);
+            $breakpoint_name       = $breakpoint_definition[0];
+            $breakpoint_parameters = explode('-', $breakpoint_definition[1]);
 
-		foreach ($this->_parameters as $data) {
-			$output = "@media ";
+            foreach( $breakpoint_parameters as $parameter ) {
+                switch (substr($parameter, -1)) {
+                    case 'H':
+                        $parameter = substr($parameter, 0, -1);
+                        $property = 'max-height';
+                    break;
 
-			$breakpoint_definition = explode(':', $data);
-			$breakpoint_name       = $breakpoint_definition[0];
-			$breakpoint_parameters = explode('-', $breakpoint_definition[1]);
+                    case 'W':
+                        $parameter = substr($parameter, 0, -1);
+                        $property = 'max-width';
+                    break;
 
-			foreach( $breakpoint_parameters as $parameter ) {
-				switch (substr($parameter, -1)) {
-					case 'H':
-						$parameter = substr($parameter, 0, -1);
-						$property = 'max-height';
-					break;
+                    case 'h':
+                        $parameter = substr($parameter, 0, -1);
+                        $property = 'min-height';
+                    break;
 
-					case 'W':
-						$parameter = substr($parameter, 0, -1);
-						$property = 'max-width';
-					break;
+                    case 'w':
+                        $parameter = substr($parameter, 0, -1);
+                        $property = 'min-width';
+                    break;
 
-					case 'h':
-						$parameter = substr($parameter, 0, -1);
-						$property = 'min-height';
-					break;
+                    default:
+                        $property = 'min-width';
+                    break;
+                }
 
-					case 'w':
-						$parameter = substr($parameter, 0, -1);
-						$property = 'min-width';
-					break;
+                $type = strtolower(substr($parameter, -2));
 
-					default:
-						$property = 'min-width';
-					break;
-				}
+                if ($type == 'px' || $type == 'em') {
+                    $value = $parameter;
+                } elseif (is_numeric($parameter)) {
+                    $value = $parameter . 'px';
+                } else {
+                    $value = $parameter;
+                }
 
-				$type = strtolower(substr($parameter, -2));
+                $output .= "({$property}: {$value}) and ";
+            }
 
-				if ($type == 'px' || $type == 'em') {
-					$value = $parameter;
-				} elseif (is_numeric($parameter)) {
-					$value = $parameter . 'px';
-				} else {
-					$value = $parameter;
-				}
+            $output = substr($output, 0, strlen($output) - 5);
+            $output .= "{\n";
+            $output .= "    body:after{\n";
+            $output .= "        content: \"$breakpoint_name\";\n";
+            $output .= "    }\n";
+            $output .= "}\n";
+            $this->_breakpoints[] = $output;
+        }
+    }
 
-				$output .= "({$property}: {$value}) and ";
-			}
+    public function getBreakpointParameters()
+    {
+        $parts = explode('|', $this->_request);
 
-			$output = substr($output, 0, strlen($output) - 5);
-			$output .= "{\n";
-			$output .= "	body:after{\n";
-			$output .= "		content: \"$breakpoint_name\";\n";
-			$output .= "	}\n";
-			$output .= "}\n";
-			$this->_breakpoints[] = $output;
-		}
-	}
+        return explode(',', $parts[0]);
+    }
 
-	public function getBreakpointParameters()
-	{
-		$parts = explode('|', $this->_request);
+    public function getThemeName($request)
+    {
+        $parts = explode('|', $this->_request);
 
-		return explode(',', $parts[0]);
-	}
+        return $parts[1];
+    }
 
-	public function getThemeName($request)
-	{
-		$parts = explode('|', $this->_request);
+    public function getTheme()
+    {
+        $theme_name = $this->_themeName;
+        if ($theme_name && key_exists($theme_name, $this->_themes)) {
+            $theme = $this->_themes[$theme_name];
+        } else {
+            $theme = $this->_themes['default'];
+        }
 
-		return $parts[1];
-	}
+        return $theme;
+    }
 
-	public function getTheme()
-	{
-		$theme_name = $this->_themeName;
-		if ($theme_name && key_exists($theme_name, $this->_themes)) {
-			$theme = $this->_themes[$theme_name];
-		} else {
-			$theme = $this->_themes['default'];
-		}
+    public function output()
+    {
+        $output = implode('', $this->_breakpoints);
 
-		return $theme;
-	}
-
-	public function output()
-	{
-		$output = implode('', $this->_breakpoints);
-
-		echo "body:after {
-	box-sizing: border-box;
-	position: fixed;
-	right: 0;
-	text-align: center;
-	top: 0;
-	z-index: 999999;
-	{$this->getTheme()}
+        echo "body:after {
+    box-sizing: border-box;
+    position: fixed;
+    right: 0;
+    text-align: center;
+    top: 0;
+    z-index: 999999;
+    {$this->getTheme()}
 }
-$output
-	";
-	}
+$output";
+    }
 }
 
 // Get Route
 $route = substr($_SERVER['REQUEST_URI'], 1);
 
 if (strpos($route,':') !== false) {
-	$css = new MediaQueryBuilder($route);
-	header("Content-Type: text/css", true);
-	echo $css->output();
+    $css = new MediaQueryBuilder($route);
+    header("Content-Type: text/css", true);
+    echo $css->output();
 } else {
-	include 'home.php';
+    include 'home.php';
 }
